6507b570782673fa4b3c828fa3479723876fdd27
crotsu <crotsu@gmail.com>
2015-11-11 16:14:10 +0900

    文字の切り出しのパラメータ調整

--- getNumberImage/getNumberImage.py
+++ getNumberImage/getNumberImage.py
@@ -1,15 +1,23 @@
 import cv2
 
-x = 187  #cols
-y = 678  #rows
+# 最初の画像の左上の切り出しポイン
+base_x = 190  #cols
+base_y = 681  #rows
 
-width = 180
-height = 180
+# 文字サイズ
+width = 177
+height = 177
 
-num = 12
+# 1枚に記録されている文字の個数
+row = 9
+col = 12
 
-for i in range(num):
-    src = cv2.imread('../image/oeda1.jpg', 1)
-    dst = src[y:y+height, x:x+width]
-    cv2.imwrite(str(i) + '.jpg', dst)
-    x += width
+y = base_y
+for i in range(row):
+    x = base_x
+    for j in range(col):
+        src = cv2.imread('../image/oeda1.jpg', 1)
+        dst = src[y:y+height, x:x+width]
+        cv2.imwrite('../result/' + str(i) + str(j) + '.jpg', dst)
+        x += width
+    y += height + 124 # 124は縦の文字と文字間の距離
