daacb11de18e106f81081d4b9bef2f0b844061b5
crotsu <crotsu@gmail.com>
2015-11-15 11:09:53 +0900

    教師信号を追加

--- getNumberImage/getNumberArea.py
+++ getNumberImage/getNumberArea.py
@@ -1,9 +1,12 @@
 # テンプレートマッチングを用いて，枠を検出する．
 import cv2
 import numpy as np
+import pandas as pd
+
+filenumber = 1
 
 # 元画像 コーナーの数は16+12*9= 124
-img = cv2.imread('../image/oeda1.jpg', 1)
+img = cv2.imread('../image/oeda' +str(filenumber) + '.jpg', 1)
 
 # テンプレート画像
 width = 50
@@ -30,16 +33,59 @@ res = cv2.matchTemplate(canvas,template,cv2.TM_CCOEFF_NORMED)
 threshold = 0.9
 loc = np.where(res >= threshold)
 corner = []
-for pt in zip(*loc[::-1]):
-    corner.append(pt)
+
+# locをソートする
+# 日付
+sortVal = []
+for (i, pt) in enumerate(zip(*loc[::-1])):
+    if i<8:
+        sortVal.append(pt)
+    else:
+        break
+sortVal.sort()
+corner.extend(sortVal)
+
+# 名前
+sortVal = []
+for (i, pt) in enumerate(zip(*loc[::-1])):
+    if 8<=i and i<16:
+        sortVal.append(pt)
+    elif 16<=i:
+        break
+sortVal.sort()
+corner.extend(sortVal)
+
+# 数字
+sortVal = []
+for (i, pt) in enumerate(zip(*loc[::-1])):
+    if 16<=i:
+        sortVal.append(pt)
+sortVal.sort()
+corner.extend(sortVal)
+
+df = pd.read_csv('../numberSheet/rand' + str(filenumber) + '.csv', header=None)
 
 # 枠領域を検出
 size = corner[1][0] - corner[0][0]
 for (i, pt) in enumerate(corner):
-    cv2.rectangle(img, (pt[0]+3, pt[1]+3), (pt[0] + size-15, pt[1] + size-15), (0,0,255), 3)
+    cv2.rectangle(img, (pt[0]+3, pt[1]+3), (pt[0] + size-15, pt[1] + size-15), (255,255,255), 3)
     dst = img[(pt[1]+3):(pt[1]+size-15), (pt[0]+3):(pt[0]+size-15)]
-    cv2.imwrite('../result/' + str(i) + '.jpg', dst)
+    if i<8:
+        filename = '../result/date' + str(i) + '.jpg'
+    elif i<16:
+        filename = '../result/name' + str(i-8) + '.jpg'
+    else:
+        filename = '../result/' + str(i-16) + '.jpg'
 
-# 出力
+    cv2.imwrite(filename, dst)
+
+# 教師信号を出力
+f = open('teach.dat', 'w')
+for col in range(12):
+    for row in range(9):
+        f.write(str(int(df[col][row]))+'\n')
+f.close()
+
+# 枠を出力
 cv2.imwrite('corner.png',img)
 
